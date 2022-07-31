13e07b03016b2ab2f8f1e2aaf85e2f5300cccf93
Vitalii Kolmakov <w.kolmakow@gmail.com>
2022-07-31 14:09:53 +0200

    minor fixes

--- bmw/carview/tasks.py
+++
@@ -1,9 +0,0 @@
-from background_task import background
-from logging import getLogger
-
-logger = getLogger(__name__)
-
-@background(schedule=3)
-def demo_task(message):
-    print('kaka')
-    logger.debug('demo_task. message={0}'.format(message))
--- bmw/tools/download.py
+++ bmw/tools/download.py
@@ -1,7 +1,9 @@
 import requests # request img from web
 import shutil # save img locally
+import os
 
 def download(url, path):
+    os.mkdir(path)
     res = requests.get(url, stream = True)
 
     if res.status_code == 200:
